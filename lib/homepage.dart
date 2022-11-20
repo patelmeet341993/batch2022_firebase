44cd52bf51b86c3a8ad762c0397932fd6bad4d53
Meet Patel <patelmeet341993@gmail.com>
2022-11-20 10:36:03 +0530

    login

---
+++ lib/homepage.dart
@@ -0,0 +1,141 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+  bool isLogin=false;
+  String name="",email="",photo="";
+
+
+  Future<void> getLogOut() async {
+    try {
+      await GoogleSignIn().signOut();
+      await FirebaseAuth.instance.signOut();
+      isLogin=false;
+      setState(() {});
+    } catch (e) {}
+  }
+
+  Future<void> checkLogin()async{
+
+
+    isLogin=FirebaseAuth.instance.currentUser!=null;
+
+    if(FirebaseAuth.instance.currentUser==null)
+      {
+        print("no user found");
+      }
+    else
+      {
+        print("user found");
+
+      User? user=  FirebaseAuth.instance.currentUser;
+
+      if(user!=null)
+        {
+          print("email : ${user.email} :  ${user.displayName} : ${user.photoURL}");
+
+          photo=user.photoURL!;
+          email=user.email!;
+          name=user.displayName!;
+        }
+      }
+
+
+  }
+
+  Future<void> getLogin() async {
+    try {
+      GoogleSignInAccount? account = await GoogleSignIn().signIn();
+      if (account == null) {
+        print("not login with any accpunt");
+      } else {
+        print("login with google");
+
+        print("Name : ${account.displayName}");
+        print("Photo : ${account.photoUrl}");
+        print("Email : ${account.email}");
+
+        photo=account.photoUrl!;
+        email=account.email;
+        name=account.displayName!;
+
+        GoogleSignInAuthentication auth=await account.authentication;
+
+        AuthCredential credential=GoogleAuthProvider.credential(
+          accessToken: auth.accessToken,
+          idToken: auth.idToken
+        );
+
+        FirebaseAuth.instance.signInWithCredential(credential);
+        isLogin=true;
+        setState(() {});
+      }
+    } catch (e) {
+      print("Error : $e");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    checkLogin();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Visibility(
+              visible: !isLogin,
+              child: InkWell(
+                onTap: () async {
+                  getLogin();
+                },
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  color: Colors.grey,
+                  child: Text("Login"),
+                ),
+              ),
+            ),
+            Visibility(
+              visible: isLogin,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.network(photo),
+                  Text("Name : $name"),
+                  Text("email : $email"),
+                  InkWell(
+                    onTap: () async {
+                      getLogOut();
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      color: Colors.grey,
+                      child: Text("Log Out"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebaseproj/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -18,71 +20,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-   return Scaffold(
-     body: Center(
-       child: InkWell(
-         onTap: ()async{
-
-           print("Click");
-           try{
-
-             GoogleSignInAccount? account=await GoogleSignIn().signIn();
-             if(account==null)
-               {
-                 print("not login with any accpunt");
-               }
-             else
-               {
-                 print("login with google");
-
-                 print("Name : ${account.displayName}");
-                 print("Photo : ${account.photoUrl}");
-                 print("Email : ${account.email}");
-
-               }
-
-           }
-           catch(e)
-           {
-             print("Error : $e");
-           }
-
-
-         },
-         child: Container(
-           padding: EdgeInsets.all(10),
-           color: Colors.grey,
-           child: Text("Login"),
-         ),
-       ),
-     ),
-   );
-  }
-}
